ee9a61cc98f81552094a16f235c29caee93f4c02
sherryfan <sherryfan706@gmail.com>
2019-01-26 18:33:41 -0500

    push all changes
    latest progress

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,6 +11,7 @@ public class Movement : MonoBehaviour
     public float speed = 1f;
     public bool stop = false;
     public GameObject spaceman, // the sprite of player
+    fart,
     direction, // the sprite for direction indication
     directionEnd, //for calculation of the movement direction
     EndGameUI;
@@ -34,21 +35,32 @@ public class Movement : MonoBehaviour
             if (Input.GetKey("space"))
             {
                 //print("space down");
-                isRotating = true;
-                if (state != State.crouch)
+                if (state == State.flying)
+                {
+                    isRotating = true;
+                }
+                else if (state != State.crouch)
                 {
                     state = State.crouch;
                     //trigger animation
+                    print("crouch");
+                    m_Anim.SetBool("Land", false);
                     m_Anim.SetBool("Crouch", true);
                 }
             }
 
             if (Input.GetKeyUp("space"))
             {
-                print("space up");
-                isRotating = false;
-
-                Fly();
+                //print("space up");
+                if (state == State.flying)
+                {
+                    isRotating = false;
+                    Fly();
+                }
+                else
+                {
+                    StartCoroutine(Launch());
+                }
 
             }
         }
@@ -62,15 +74,23 @@ public class Movement : MonoBehaviour
                 {
                     state = State.crouch;
                     //trigger animation
+                    m_Anim.SetBool("Land", false);
                     m_Anim.SetBool("Crouch", true);
                 }
             }
 
             if (Input.GetKeyUp(KeyCode.Return))
             {
-                print("return up");
-                isRotating = false;
-                Fly();
+                //print("return up");
+                if (state == State.flying)
+                {
+                    isRotating = false;
+                    Fly();
+                }
+                else
+                {
+                    StartCoroutine(Launch());
+                }
             }
         }
 
@@ -87,6 +107,7 @@ public class Movement : MonoBehaviour
 
     void Fly()
     {
+        StartCoroutine(Fart());
         Vector2 flyDirection = spaceman.transform.position - directionEnd.transform.position;
         rb.AddForce(flyDirection * speed);
         if (state != State.flying)
@@ -102,8 +123,31 @@ public class Movement : MonoBehaviour
             spaceman.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         }
-
         print("previous velocity: " + GetComponent<Rigidbody2D>().velocity.magnitude);
+    }
+
+    IEnumerator Fart(){
+        fart.SetActive(true);
+        fart.GetComponent<Animator>().SetTrigger("Fart");
+        direction.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
+        yield return new WaitForSeconds(1.2f);
+        fart.SetActive(false);
+        direction.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+        yield return null;
+    }
+
+    IEnumerator Launch()
+    {
+        rb.AddForce(spaceman.transform.up * speed);
+
+        state = State.flying;
+        //trigger animation
+        m_Anim.SetBool("Crouch", false);
+        m_Anim.SetBool("Jump", true);
+
+        yield return new WaitForSeconds(1f);
+        direction.SetActive(true);
+        yield return null;
     }
 
     private void OnCollisionEnter2D(Collision2D other)
@@ -112,30 +156,32 @@ public class Movement : MonoBehaviour
         {
             rb.velocity = Vector2.zero;
             state = State.idle;
-            
+            m_Anim.SetBool("Jump", false);
+            m_Anim.SetBool("Land", true);
+
             // adjust to perpendicular position
             if (other.contacts[0].normal.y >= 0.9f)
             {
-                Vector3 angle = new Vector3 (0f, 0f, 0f);
+                Vector3 angle = new Vector3(0f, 0f, 0f);
                 spaceman.transform.rotation = Quaternion.Euler(angle);
             }
-            else if(other.contacts[0].normal.y <= -0.9f)
+            else if (other.contacts[0].normal.y <= -0.9f)
             {
                 Vector3 angle = new Vector3(0f, 0f, 180f);
                 spaceman.transform.rotation = Quaternion.Euler(angle);
             }
-            else if(other.contacts[0].normal.x >= 0.9f)
+            else if (other.contacts[0].normal.x >= 0.9f)
             {
                 Vector3 angle = new Vector3(0f, 0f, 270f);
                 spaceman.transform.rotation = Quaternion.Euler(angle);
             }
-            else if(other.contacts[0].normal.x <= -0.9f)
+            else if (other.contacts[0].normal.x <= -0.9f)
             {
                 Vector3 angle = new Vector3(0f, 0f, 90f);
                 spaceman.transform.rotation = Quaternion.Euler(angle);
             }
 
-            
+
 
         }
 
