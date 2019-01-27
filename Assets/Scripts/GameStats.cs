9bf3c920b5f1cdbba1045e5253b5160d9c126306
sherryfan <sherryfan706@gmail.com>
2019-01-27 03:26:13 -0500

    finish updating animations
    new animation
    hopefully final

---
+++ Assets/Scripts/GameStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameStats
+{
+    // Start is called before the first frame update
+    private static string playerA, playerB;
+    private static int level = 0, day = 0; 
+}
