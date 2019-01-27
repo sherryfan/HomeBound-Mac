df4cbe5ca2f282f21950e896f90670b472eeb1c7
Xuhao Du <>
2019-01-27 03:31:07 -0500

    blackhole attraction

---
+++ Assets/Scripts/BlackholeAttract.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeAttract : MonoBehaviour
+{
+    public GameObject blackhole;
+
+    public Vector2 direction;
+
+    public float blackhole_velocity = 0.5f;
+
+    // Start is called before the first frame update
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.name == "SpacemanSpriteA")
+        {
+            direction = blackhole.transform.position - other.gameObject.transform.position;
+            other.gameObject.transform.parent.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * blackhole_velocity);
+        }
+        else if(other.gameObject.name == "SpacemanSpriteB")
+        {
+            direction = blackhole.transform.position - other.gameObject.transform.position;
+            other.gameObject.transform.parent.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * blackhole_velocity);
+        }
+    }
+}
